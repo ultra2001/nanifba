2936e2fe97e3d133c94f4656c77dbaf24d9ae0ae
ultra2001 <77781196+ultra2001@users.noreply.github.com>
2021-06-21 23:28:27 +0100

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,39 +1,34 @@
-import { Avatar } from "@material-ui/core";
+
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 import styled from "styled-components";
 import { auth, db } from "../firebase";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import getRecipientEmail from "../utils/getRecipientEmail";
 
-function Chat({ id, users}) {
-   
+function Chat({ id, users }) {
+
     const router = useRouter();
     const [user] = useAuthState(auth);
     const [receiverSnapshot] = useCollection(
-        db.collection("users").where("email", '==', getRecipientEmail(users,user))
-        ); 
+        db.collection("users").where("email", '==', getRecipientEmail(users, user))
+    );
 
-        const enterChat = () => {
-            router.push(`/chat/${id}`)
-        }
-    
+    const enterChat = () => {
+        router.push(`/chat/${id}`)
+    }
+
     const recipient = receiverSnapshot?.docs?.[0]?.data();
     const recipientEmail = getRecipientEmail(users, user);
-    
+
     return (
         <Container onClick={enterChat}>
 
-        {recipient ?(
-            <UserAvatar src={recipient?.photoURl} />
-        ): (
-            <UserAvatar> {recipientEmail[0]} </UserAvatar> 
-          
-        )}
-        
 
 
-<p> {recipientEmail} </p> 
+
+
+            <p> {recipientEmail} </p>
 
 
         </Container>
@@ -55,10 +50,5 @@ word-break: break-word;
 }
 `;
 
-const UserAvatar = styled(Avatar)`
-margin: 5px;
-margin-right: 15px;
-border: 1px Solid black;
 
-`;
 
