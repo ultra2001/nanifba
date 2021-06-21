f35fe2054e82d6117a9bb017f35b64f23b124530
ultra2001 <77781196+ultra2001@users.noreply.github.com>
2021-06-21 23:16:54 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,27 @@
+// next.config.js
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      // issuer: {
+      //   test: /\.(js|ts)x?$/,
+      // },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeViewBox: false,
+              },
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
