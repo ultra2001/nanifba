c1f91415a5f086cde434db23b5e78ce236a8ba03
ultra2001 <77781196+ultra2001@users.noreply.github.com>
2021-06-21 23:58:25 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Chat App</title>
+
+      </Head>
+      <Sidebar />
+    </div>
+  )
+}
